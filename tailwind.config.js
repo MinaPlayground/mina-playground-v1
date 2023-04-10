2942cac9a25ae356a6208c6ebb3eb2708acc953d
devarend <devarend@gmail.com>
2023-04-10 23:21:14 +0200

    poc for interactive tutorials

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
