b0c97c60b412e9aa32b3701b8808c4d516fe54c6
devarend <devarend@gmail.com>
2023-04-30 01:10:52 +0200

    added directory and setDirectory call backs to Tree

--- src/components/file-explorer/Tree.tsx
+++ src/components/file-explorer/Tree.tsx
@@ -2,11 +2,25 @@ import { FC } from "react";
 import TreeNode from "@/components/file-explorer/TreeNode";
 import type { FileSystemTree } from "@webcontainer/api";
 
-const Tree: FC<TreeProps> = ({ data, onBlur }) => {
+const Tree: FC<TreeProps> = ({
+  data,
+  onBlur,
+  setCurrentDirectory,
+  directory = [],
+  currentDirectory,
+}) => {
   return (
     <ul>
       {Object.entries(data).map((node, index) => (
-        <TreeNode node={node} key={index} onBlur={onBlur} />
+        <TreeNode
+          node={node}
+          key={index}
+          index={index}
+          onBlur={onBlur}
+          directory={directory}
+          currentDirectory={currentDirectory}
+          setCurrentDirectory={setCurrentDirectory}
+        />
       ))}
     </ul>
   );
@@ -15,6 +29,9 @@ const Tree: FC<TreeProps> = ({ data, onBlur }) => {
 interface TreeProps {
   data: FileSystemTree;
   onBlur(): void;
+  setCurrentDirectory(directory: string[]): void;
+  directory?: string[];
+  currentDirectory: string[];
 }
 
 export default Tree;
