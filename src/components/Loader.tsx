433d31cbd0aa9aa6c2b6e6257b0b77d393636f7c
devarend <devarend@gmail.com>
2023-05-04 14:08:12 +0200

    Loader now supports text

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,10 +2,10 @@ import { FC } from "react";
 
 const Loader: FC<LoaderProps> = ({ text }) => {
   return (
-    <div role="status">
+    <div className="flex flex-row items-center" role="status">
       <svg
         aria-hidden="true"
-        className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        className="w-8 h-8 mr-2 text-gray-400 animate-spin fill-orange-600"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
@@ -19,7 +19,7 @@ const Loader: FC<LoaderProps> = ({ text }) => {
           fill="currentFill"
         />
       </svg>
-      <span className="sr-only">{text ? text : "Loading"}...</span>
+      <span>{text ? text : "Loading"}...</span>
     </div>
   );
 };
