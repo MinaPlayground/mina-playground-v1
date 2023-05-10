79c39f618aaeef0f963cd3fbd40de216290b467e
devarend <devarend@gmail.com>
2023-05-10 16:28:40 +0200

    configure mdx and set custom components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,26 @@ import { store } from "@/store";
 import "@/styles/editor.scss";
 import "flowbite";
 import "xterm/css/xterm.css";
+import { MDXProvider } from "@mdx-js/react";
+import TutorialHeader from "@/components/tutorial/TutorialHeader";
+import TutorialText from "@/components/tutorial/TutorialText";
+import Codeblock from "@/components/tutorial/Codeblock";
+import TutorialHighlightedText from "@/components/tutorial/TutorialHighlightedText";
+import { Components } from "@mdx-js/react/lib";
+
+const components: Components = {
+  h1: TutorialHeader,
+  p: TutorialText,
+  pre: Codeblock,
+  strong: TutorialHighlightedText,
+};
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <MDXProvider components={components}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </MDXProvider>
   );
 }
