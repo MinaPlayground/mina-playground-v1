4a35c1b14a6e1ef0898b97c27b3fda0730174ff6
devarend <devarend@gmail.com>
2023-05-20 18:41:50 +0200

    add test and src file to tutorials file instead of breadcrumb items, fix lazyloading flickering bug by returning the outerHTML while loading

--- parseTutorials.js
+++ parseTutorials.js
@@ -28,21 +28,28 @@ const parseTutorials = (async () => {
       });
       const sectionSrcFilesPath = path.join(sectionPath, "src");
       const sectionTestsPath = path.join(sectionPath, "tests");
-      const baseSrcFilesPath = path.join(
-        "./src/tutorials",
-        item,
-        "base",
-        "src"
-      );
-      const baseTestsPath = path.join("./src/tutorials", item, "base", "tests");
 
-      const tests = await fs.readdir(sectionTestsPath);
+      const basePath = path.join("./src/tutorials", item, "base");
+      const baseSrcFilesPath = path.join(basePath, "src");
+      const baseTestsPath = path.join(basePath, "tests");
+
+      const test = await fs.readdir(sectionTestsPath);
       data[key].sections.push({
         name: JSON.parse(metaFileContent).name,
-        tests,
       });
 
-      imports += `C${key}S${sectionKey}: dynamic(() => import(\"${sectionTutorialPath}\")),`;
+      const sectionSrcFile = await fs.readdir(path.join(sectionSrcFilesPath));
+      const srcFilePath = path.join(
+        sectionSrcFilesPath,
+        sectionSrcFile.toString()
+      );
+      const scrFile = await fs.readFile(srcFilePath, {
+        encoding: "utf-8",
+      });
+
+      imports += `C${key}S${sectionKey}: {tutorial: dynamic(() => import(\"${sectionTutorialPath}\"), {loading}), test: "${test}", src: ${JSON.stringify(
+        scrFile
+      )}},`;
 
       await fs.cp(
         sectionSrcFilesPath,
@@ -67,9 +74,19 @@ const parseTutorials = (async () => {
     }
   }
   await fs.writeFile(
-    "./src/tutorials.ts",
+    "./src/tutorials.tsx",
     `import dynamic from "next/dynamic";
-
+const loading = () => {
+  const lazyEl = document.getElementById("tutorial")?.outerHTML;
+  return (
+    <div
+      style={{ display: "contents" }}
+      dangerouslySetInnerHTML={{
+        __html: lazyEl ? lazyEl : "",
+      }}
+    />
+  );
+};
 export const dynamicComponents = {${imports}};`
   );
 })();
