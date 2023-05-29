8f4c72e84cd13b4aa7157ff933028d82e92e69c8
devarend <devarend@gmail.com>
2023-05-29 22:07:16 +0200

    created util to transform to webcontainer files with focused files

--- src/utils/webcontainer.ts
+++ src/utils/webcontainer.ts
@@ -26,3 +26,50 @@ export const transformToWebcontainerFiles = async (
   }
   return files;
 };
+
+const dirs = [
+  "src/tutorials/01-introduction/01-smart-contracts/src/Add.ts",
+  "src/tutorials/01-introduction/01-smart-contracts/src/test/Test2.ts",
+];
+
+export const transformToWebcontainerFilesWithFocus = async (
+  dir: string,
+  files: Record<string, any> = {},
+  focusedFiles: Record<string, any> = {}
+) => {
+  const projectDir = await fs.readdir(dir);
+  for (const item of projectDir) {
+    const itemPath = path.join(dir, item);
+    const stats = await fs.stat(itemPath);
+    if (stats.isDirectory()) {
+      files[item] = {
+        directory: {},
+      };
+      focusedFiles[item] = {
+        directory: {},
+      };
+      await transformToWebcontainerFilesWithFocus(
+        itemPath,
+        files[item].directory,
+        focusedFiles[item].directory
+      );
+    } else {
+      const fileName = path.basename(item);
+      const fileContent = await fs.readFile(itemPath, { encoding: "utf-8" });
+      if (dirs.includes(itemPath)) {
+        focusedFiles[fileName] = {
+          file: {
+            contents: fileContent,
+          },
+        };
+      } else {
+        files[fileName] = {
+          file: {
+            contents: fileContent,
+          },
+        };
+      }
+    }
+  }
+  return { files, focusedFiles };
+};
