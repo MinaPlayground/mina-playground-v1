7a3d5e8ca9feaf06001580efe3b70535835adce0
devarend <devarend@gmail.com>
2023-05-31 17:26:34 +0200

    added test

---
+++ src/pages/api/test.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getTutorialByChapterAndSection } from "@/utils/tutorial";
+import { isValidChapterAndSection } from "@/utils/validation";
+
+type Error = {
+  message: string;
+};
+
+type Data = {
+  files: Record<string, any>;
+};
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<Error | Data>
+) => {
+  if (req.method !== "POST") {
+    res.status(405).send({ message: "Only POST requests allowed" });
+    return;
+  }
+  const isValid = isValidChapterAndSection(req.body.chapter, req.body.section);
+  if (!isValid) {
+    res.status(422).json({ message: "Chapter and/or section is not correct" });
+    return;
+  }
+  const response = await getTutorialByChapterAndSection(
+    req.body.chapter,
+    req.body.section
+  );
+  res.status(200).json({ ...response });
+};
+
+export default handler;
