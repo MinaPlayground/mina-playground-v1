8289d638d76e49324c85f52eb8f0e14e9874745d
devarend <devarend@gmail.com>
2023-06-16 22:28:19 +0200

    create project functionality in projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,109 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
 
-const Projects: FC = () => {
+const Projects: FC<ProjectProps> = ({ data }) => {
+  const [name, setName] = useState("");
+  const router = useRouter();
+  const createProject = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/project",
+        { name, type: 0, visibility: true, files_id: 1 },
+        { headers: { "Content-Type": "application/json" } }
+      );
+      router.reload();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
-      <h2 className="text-4xl font-semibold p-4">Projects</h2>
+      <div className="flex items-center">
+        <h2 className="text-4xl font-semibold p-4">Projects</h2>
+        <div>
+          <button
+            data-modal-target="authentication-modal"
+            data-modal-toggle="authentication-modal"
+            className="flex text-white bg-gradient-to-br from-pink-500 to-orange-400 font-medium rounded-lg text-sm px-5 py-2.5 items-center"
+            type="button"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-4 h-4 mr-2"
+              fill={"#FFF"}
+              viewBox="0 0 448 512"
+            >
+              <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+            </svg>
+            Create project
+          </button>
+        </div>
+      </div>
+      <div
+        id="authentication-modal"
+        tabIndex={-1}
+        aria-hidden="true"
+        className="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
+      >
+        <div className="relative w-full max-w-md max-h-full">
+          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <button
+              type="button"
+              className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+              data-modal-hide="authentication-modal"
+            >
+              <svg
+                aria-hidden="true"
+                className="w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <span className="sr-only">Close modal</span>
+            </button>
+            <div className="px-6 py-6 lg:px-8">
+              <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
+                Create a new project
+              </h3>
+              <form className="space-y-6" action="#">
+                <div>
+                  <label
+                    htmlFor="name"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    value={name}
+                    onChange={(evt) => setName(evt.target.value)}
+                    id="name"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                    placeholder="My zkApp project"
+                    required
+                  />
+                </div>
+                <button
+                  type="button"
+                  onClick={createProject}
+                  className="w-full text-white bg-gradient-to-br from-pink-500 to-orange-400 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                >
+                  Create
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg mx-4">
         <table className="w-full text-sm text-left text-gray-500">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50">
@@ -23,105 +123,47 @@ const Projects: FC = () => {
             </tr>
           </thead>
           <tbody>
-            <tr className="bg-white border-b hover:bg-gray-50">
-              <th
-                scope="row"
-                className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap"
+            {data.map(({ _id, name, type, visibility, files_id }, key) => (
+              <tr
+                onClick={() => router.push(`/project/${_id}`)}
+                key={key}
+                className="bg-white border-b hover:bg-gray-50 cursor-pointer"
               >
-                <img
-                  className="w-10 h-10 rounded-full"
-                  src="./react.svg"
-                  alt="react"
-                />
-                <div className="pl-3">
-                  <div className="text-base font-semibold">Test zkApp</div>
-                </div>
-              </th>
-              <td className="px-6 py-4">zkApp</td>
-              <td className="px-6 py-4">
-                <div className="flex items-center">
-                  <div className="h-2.5 w-2.5 rounded-full bg-green-500 mr-2" />
-                  Public
-                </div>
-              </td>
-              <td className="px-6 py-4">
-                <a
-                  href="#"
-                  type="button"
-                  data-modal-target="editUserModal"
-                  data-modal-show="editUserModal"
-                  className="font-medium text-blue-600 hover:underline"
+                <th
+                  scope="row"
+                  className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap"
                 >
-                  Edit project
-                </a>
-              </td>
-            </tr>
-            <tr className="bg-white border-b hover:bg-gray-50">
-              <th
-                scope="row"
-                className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap"
-              >
-                <img
-                  className="w-10 h-10 rounded-full"
-                  src="./ts.svg"
-                  alt="ts"
-                />
-                <div className="pl-3">
-                  <div className="text-base font-semibold">Test contract</div>
-                </div>
-              </th>
-              <td className="px-6 py-4">Smart Contract</td>
-              <td className="px-6 py-4">
-                <div className="flex items-center">
-                  <div className="h-2.5 w-2.5 rounded-full bg-green-500 mr-2" />
-                  Public
-                </div>
-              </td>
-              <td className="px-6 py-4">
-                <a
-                  href="#"
-                  type="button"
-                  data-modal-target="editUserModal"
-                  data-modal-show="editUserModal"
-                  className="font-medium text-blue-600 hover:underline"
-                >
-                  Edit project
-                </a>
-              </td>
-            </tr>
-            <tr className="bg-white border-b hover:bg-gray-50">
-              <th
-                scope="row"
-                className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap"
-              >
-                <img
-                  className="w-10 h-10 rounded-full"
-                  src="./react.svg"
-                  alt="react"
-                />
-                <div className="pl-3">
-                  <div className="text-base font-semibold">Test zkApp</div>
-                </div>
-              </th>
-              <td className="px-6 py-4">zkApp</td>
-              <td className="px-6 py-4">
-                <div className="flex items-center">
-                  <div className="h-2.5 w-2.5 rounded-full bg-gray-400 mr-2" />
-                  Private
-                </div>
-              </td>
-              <td className="px-6 py-4">
-                <a
-                  href="#"
-                  type="button"
-                  data-modal-target="editUserModal"
-                  data-modal-show="editUserModal"
-                  className="font-medium text-blue-600 hover:underline"
-                >
-                  Edit project
-                </a>
-              </td>
-            </tr>
+                  <img
+                    className="w-10 h-10 rounded-full"
+                    src="./react.svg"
+                    alt="react"
+                  />
+                  <div className="pl-3">
+                    <div className="text-base font-semibold">{name}</div>
+                  </div>
+                </th>
+                <td className="px-6 py-4">
+                  {type === 0 ? "zkApp" : "Smart Contract"}
+                </td>
+                <td className="px-6 py-4">
+                  <div className="flex items-center">
+                    <div className="h-2.5 w-2.5 rounded-full bg-green-500 mr-2" />
+                    Public
+                  </div>
+                </td>
+                <td className="px-6 py-4">
+                  <a
+                    href="#"
+                    type="button"
+                    data-modal-target="editUserModal"
+                    data-modal-show="editUserModal"
+                    className="font-medium text-blue-600 hover:underline"
+                  >
+                    Edit project
+                  </a>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
         <div
@@ -191,5 +233,15 @@ const Projects: FC = () => {
     </>
   );
 };
+
+interface ProjectProps {
+  data: {
+    _id: string;
+    name: string;
+    type: number;
+    visibility: boolean;
+    files_id: number;
+  }[];
+}
 
 export default Projects;
