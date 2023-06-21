9ad8690155b565bcb65107167ca73947fae2c332
devarend <devarend@gmail.com>
2023-06-21 23:42:42 +0200

    file creation demo with immer

--- src/pages/project/[projectId].tsx
+++ src/pages/project/[projectId].tsx
@@ -11,7 +11,8 @@ import Loader from "@/components/Loader";
 import TestSection from "@/components/test/TestSection";
 import TerminalOutput from "@/components/terminal/TerminalOutput";
 import SelectList from "@/components/select/SelectList";
-import { isEmpty } from "lodash";
+import { get, isEmpty } from "lodash";
+import { produce } from "immer";
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   const { projectId } = query;
@@ -47,6 +48,7 @@ const Home: NextPage<HomeProps> = ({ fileSystemTree, name, _id }) => {
   const inputRef = useRef<any>(null);
   const shellRef = useRef<any>(null);
   const [terminalOutput, setTerminalOutput] = useState<boolean | null>(null);
+  const [fileData, setFileData] = useState<FileSystemTree>(fileSystemTree);
 
   const [changedFields, setChangedFields] = useState({});
 
@@ -146,7 +148,7 @@ const Home: NextPage<HomeProps> = ({ fileSystemTree, name, _id }) => {
 
   const initialize = async () => {
     setIsInitializing(true);
-    await startWebContainer();
+    // await startWebContainer();
   };
 
   useEffect(() => {
@@ -167,6 +169,48 @@ const Home: NextPage<HomeProps> = ({ fileSystemTree, name, _id }) => {
     return parsedJSON.scripts;
   };
 
+  const createNewFile = () => {
+    // const data = get(fileData, directory.webcontainerPath);
+    setFileData(
+      produce((data) => {
+        data[""] = {
+          ...data.directory,
+          "": {
+            file: {
+              contents: "",
+            },
+          },
+        };
+      })
+    );
+  };
+
+  const onBlur = (value: string, type: any) => {
+    const newFolder = {
+      directory: {},
+    };
+
+    const newFile = {
+      file: {
+        contents: "test",
+      },
+    };
+
+    const newValue = type === "directory" ? newFolder : newFile;
+
+    setFileData(
+      produce((fileData) => {
+        if (value) {
+          (fileData as FileSystemTree)[value] = newValue;
+        }
+        delete (fileData as FileSystemTree)[""];
+      })
+    );
+  };
+
+  const createNewFolder = () => {
+    setDirectory({ path: "src", webcontainerPath: "" });
+  };
   return (
     <>
       <Head>
@@ -180,9 +224,31 @@ const Home: NextPage<HomeProps> = ({ fileSystemTree, name, _id }) => {
         <div className="flex flex-1">
           <div className="p-2 flex-1">
             <h1 className="text-2xl font-medium">Project: {name}</h1>
+            <div className="flex flex-row gap-1 mb-2">
+              <svg
+                onClick={createNewFile}
+                height={16}
+                width={16}
+                className="cursor-pointer"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 576 512"
+              >
+                <path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384v38.6C310.1 219.5 256 287.4 256 368c0 59.1 29.1 111.3 73.7 143.3c-3.2 .5-6.4 .7-9.7 .7H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128zm48 96a144 144 0 1 1 0 288 144 144 0 1 1 0-288zm16 80c0-8.8-7.2-16-16-16s-16 7.2-16 16v48H368c-8.8 0-16 7.2-16 16s7.2 16 16 16h48v48c0 8.8 7.2 16 16 16s16-7.2 16-16V384h48c8.8 0 16-7.2 16-16s-7.2-16-16-16H448V304z" />
+              </svg>
+              <svg
+                onClick={createNewFolder}
+                height={16}
+                width={16}
+                className="cursor-pointer"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <path d="M512 416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96C0 60.7 28.7 32 64 32H192c20.1 0 39.1 9.5 51.2 25.6l19.2 25.6c6 8.1 15.5 12.8 25.6 12.8H448c35.3 0 64 28.7 64 64V416zM232 376c0 13.3 10.7 24 24 24s24-10.7 24-24V312h64c13.3 0 24-10.7 24-24s-10.7-24-24-24H280V200c0-13.3-10.7-24-24-24s-24 10.7-24 24v64H168c-13.3 0-24 10.7-24 24s10.7 24 24 24h64v64z" />
+              </svg>
+            </div>
             <Tree
-              data={fileSystemTree}
-              onBlur={() => null}
+              data={fileData}
+              onBlur={onBlur}
               onClick={onClick}
               setCurrentDirectory={setDirectory}
               currentDirectory={directory}
