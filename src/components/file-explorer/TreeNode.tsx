fc362a6c211892a5ed2e56f6fc18fb13348b0be1
devarend <devarend@gmail.com>
2023-06-27 23:45:10 +0200

    added create dir on focus

--- src/components/file-explorer/TreeNode.tsx
+++ src/components/file-explorer/TreeNode.tsx
@@ -64,29 +64,46 @@ const TreeNode: FC<TreeNodeProps> = ({
     <>
       <div
         onClick={handleClick}
-        className={`flex p-2 flex-row items-center mb-2 cursor-pointer ${isSelectedStyle}`}
+        className={`group new-file flex p-2 gap-1 flex-row items-center mb-2 cursor-pointer ${isSelectedStyle}`}
       >
-        {isDirectory ? (
-          showChildren ? (
-            <span className="pr-2 text-sm">▼</span>
+        <div className="flex flex-1 flex-row items-center">
+          {isDirectory ? (
+            showChildren ? (
+              <span className="text-sm">▼</span>
+            ) : (
+              <span className="text-sm">►</span>
+            )
+          ) : null}
+          {isDirectory ? <DirectoryIcon /> : <FileIcon />}
+          {key === "" ? (
+            <input
+              autoFocus
+              value={inputValue}
+              onChange={(event) => setInputValue(event.target.value)}
+              onBlur={() =>
+                onBlur(inputValue, isDirectory ? "directory" : "file")
+              }
+              className="pl-2 border border-gray-300 rounded-md bg-gray-50"
+            />
           ) : (
-            <span className="pr-1 text-sm">►</span>
-          )
-        ) : null}
-        {isDirectory ? <DirectoryIcon /> : <FileIcon />}
-        {key === "" ? (
-          <input
-            autoFocus
-            value={inputValue}
-            onChange={(event) => setInputValue(event.target.value)}
-            onBlur={() =>
-              onBlur(inputValue, isDirectory ? "directory" : "file")
-            }
-            className="pl-2 border border-gray-300 rounded-md bg-gray-50"
-          />
-        ) : (
-          <span>{key.replace(/\*/g, ".")}</span>
-        )}
+            <span>{key.replace(/\*/g, ".")}</span>
+          )}
+        </div>
+        <div className="hidden group-hover:block">
+          <svg
+            onClick={(event) => {
+              event.stopPropagation();
+              console.log(webcontainerPath);
+            }}
+            height={16}
+            width={16}
+            className="cursor-pointer"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 576 512"
+          >
+            <path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384v38.6C310.1 219.5 256 287.4 256 368c0 59.1 29.1 111.3 73.7 143.3c-3.2 .5-6.4 .7-9.7 .7H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128zm48 96a144 144 0 1 1 0 288 144 144 0 1 1 0-288zm16 80c0-8.8-7.2-16-16-16s-16 7.2-16 16v48H368c-8.8 0-16 7.2-16 16s7.2 16 16 16h48v48c0 8.8 7.2 16 16 16s16-7.2 16-16V384h48c8.8 0 16-7.2 16-16s-7.2-16-16-16H448V304z" />
+          </svg>
+        </div>
       </div>
       {isDirectory && (
         <ul className="pl-2">
