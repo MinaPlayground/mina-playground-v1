67021968610cd992398df295ab0da82c8a896afa
devarend <devarend@gmail.com>
2023-06-30 22:09:09 +0200

    created types for file system action mapper

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,23 @@ import { ParsedUrlQuery } from "querystring";
 
 export type FileSystemType = "directory" | "file";
 
+export type MapFileSystemAction = {
+  file: {
+    action(data: object): void;
+  };
+  directory: {
+    action(data: object): void;
+  };
+};
+
+export type MapFileSystemActions = {
+  create: MapFileSystemAction;
+  delete: MapFileSystemAction;
+  rename: MapFileSystemAction;
+};
+
+export type FileSystemAction = keyof MapFileSystemActions;
+
 export interface TutorialParams extends ParsedUrlQuery {
   chapter: string;
   section: string;
