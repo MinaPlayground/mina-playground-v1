eea418cfe999ffb49fbb72b4913177923da6fc13
devarend <devarend@gmail.com>
2023-07-08 22:05:31 +0200

    separated project file explorer from project page

--- src/pages/project/[projectId].tsx
+++ src/pages/project/[projectId].tsx
@@ -4,30 +4,18 @@ import Header from "@/components/Header";
 import { GetServerSideProps, NextPage } from "next";
 import axios from "axios";
 import { FileNode, FileSystemTree, WebContainer } from "@webcontainer/api";
-import Tree from "@/components/file-explorer/Tree";
 import { useEffect, useRef, useState } from "react";
 import CodeEditor from "@/components/editor/CodeEditor";
 import Loader from "@/components/Loader";
 import TestSection from "@/components/test/TestSection";
 import TerminalOutput from "@/components/terminal/TerminalOutput";
 import SelectList from "@/components/select/SelectList";
-import { get, isEmpty } from "lodash";
-import { produce } from "immer";
-import {
-  FileSystemAction,
-  FileSystemOnChangePayload,
-  FileSystemType,
-} from "@/types";
-import { mapFileSystemAction } from "@/mappers/mapFileSystemAction";
-import {
-  mutateFileTreeCreateNew,
-  mutateFileTreeOnBlur,
-} from "@/mutations/fileTreeMutations";
+import { isEmpty } from "lodash";
 import {
   useDeleteFileTreeItemMutation,
   useUpdateFileTreeMutation,
 } from "@/services/fileTree";
-import { getCombinedPathName } from "@/utils/fileSystemWeb";
+import ProjectFileExplorer from "@/components/file-explorer/ProjectFileExplorer";
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   const { projectId } = query;
@@ -63,7 +51,6 @@ const Home: NextPage<HomeProps> = ({ fileSystemTree, name, _id }) => {
   const inputRef = useRef<any>(null);
   const shellRef = useRef<any>(null);
   const [terminalOutput, setTerminalOutput] = useState<boolean | null>(null);
-  const [fileData, setFileData] = useState<FileSystemTree>(fileSystemTree);
   const [changedFields, setChangedFields] = useState({});
 
   const [updateFileTree, { isLoading }] = useUpdateFileTreeMutation();
@@ -83,10 +70,6 @@ const Home: NextPage<HomeProps> = ({ fileSystemTree, name, _id }) => {
 
   const notSaved = directory.webcontainerPath in changedFields;
 
-  const onClick = async (code: string, dir: string) => {
-    setCodeChange(code, dir);
-  };
-
   const save = async () => {
     const body = { location: directory.webcontainerPath, code };
     updateFileTree({
@@ -95,7 +78,7 @@ const Home: NextPage<HomeProps> = ({ fileSystemTree, name, _id }) => {
     });
     webcontainerInstance.current?.fs.writeFile(
       directory.path.replace(/\*/g, "."),
-      code
+      code || ""
     );
   };
 
@@ -192,70 +175,6 @@ const Home: NextPage<HomeProps> = ({ fileSystemTree, name, _id }) => {
     return parsedJSON.scripts;
   };
 
-  const createNewFile = () => {
-    setFileData(
-      produce((fileData: FileSystemTree) => {
-        mutateFileTreeCreateNew(fileData, "file");
-      })
-    );
-  };
-
-  const onChange = (
-    action: FileSystemAction,
-    type: FileSystemType,
-    payload: FileSystemOnChangePayload
-  ) => {
-    setFileData(
-      produce((fileData: FileSystemTree) => {
-        mapFileSystemAction(action, type).action(fileData, payload);
-        if (action === "delete") {
-          const location = getCombinedPathName(
-            payload.key as string,
-            payload.path
-          );
-          deleteFileTreeItem({ id: _id, body: { location } });
-        }
-      })
-    );
-  };
-
-  const onBlur = async (
-    action: "create" | "rename",
-    payload: {
-      path: string;
-      fullPath: string;
-      key: string;
-      value: string;
-    }
-  ) => {
-    const { path, fullPath, key, value } = payload;
-    setFileData(
-      produce((fileData) => {
-        mutateFileTreeOnBlur(fileData, payload);
-      })
-    );
-    const isCreateAction = action === "create";
-    const body = isCreateAction
-      ? { location: fullPath }
-      : {
-          location: getCombinedPathName(key as string, path),
-          rename: getCombinedPathName(value, path),
-        };
-
-    updateFileTree({
-      id: _id,
-      body,
-    });
-  };
-
-  const createNewFolder = () => {
-    setFileData(
-      produce((fileData: FileSystemTree) => {
-        mutateFileTreeCreateNew(fileData, "directory");
-      })
-    );
-  };
-
   return (
     <>
       <Head>
@@ -269,36 +188,7 @@ const Home: NextPage<HomeProps> = ({ fileSystemTree, name, _id }) => {
         <div className="flex flex-1 flex-col md:flex-row">
           <div className="p-2 flex-1 bg-gray-50">
             <h1 className="text-2xl font-medium">Project: {name}</h1>
-            <div className="flex flex-row gap-1 mb-2">
-              <svg
-                onClick={createNewFile}
-                height={16}
-                width={16}
-                className="cursor-pointer"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 576 512"
-              >
-                <path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384v38.6C310.1 219.5 256 287.4 256 368c0 59.1 29.1 111.3 73.7 143.3c-3.2 .5-6.4 .7-9.7 .7H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128zm48 96a144 144 0 1 1 0 288 144 144 0 1 1 0-288zm16 80c0-8.8-7.2-16-16-16s-16 7.2-16 16v48H368c-8.8 0-16 7.2-16 16s7.2 16 16 16h48v48c0 8.8 7.2 16 16 16s16-7.2 16-16V384h48c8.8 0 16-7.2 16-16s-7.2-16-16-16H448V304z" />
-              </svg>
-              <svg
-                onClick={createNewFolder}
-                height={16}
-                width={16}
-                className="cursor-pointer"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-              >
-                <path d="M512 416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96C0 60.7 28.7 32 64 32H192c20.1 0 39.1 9.5 51.2 25.6l19.2 25.6c6 8.1 15.5 12.8 25.6 12.8H448c35.3 0 64 28.7 64 64V416zM232 376c0 13.3 10.7 24 24 24s24-10.7 24-24V312h64c13.3 0 24-10.7 24-24s-10.7-24-24-24H280V200c0-13.3-10.7-24-24-24s-24 10.7-24 24v64H168c-13.3 0-24 10.7-24 24s10.7 24 24 24h64v64z" />
-              </svg>
-            </div>
-            <Tree
-              data={fileData}
-              onBlur={onBlur}
-              onChange={onChange}
-              onClick={onClick}
-              setCurrentDirectory={setDirectory}
-              currentDirectory={directory}
-            />
+            <ProjectFileExplorer fileSystemTree={fileSystemTree} id={_id} />
           </div>
           <div className="flex flex-col flex-[4]">
             <div className="ml-8 my-2 cursor-pointer" onClick={save}>
