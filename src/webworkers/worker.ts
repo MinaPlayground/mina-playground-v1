695eb08daa998c2e779b03f0d05b66d6664b5fe3
devarend <devarend@gmail.com>
2023-07-29 03:29:21 +0200

    created webworker using comlink to generate keys with snarkyjs

---
+++ src/webworkers/worker.ts
@@ -0,0 +1,13 @@
+import { expose } from "comlink";
+
+const generateKeys = async (customKeyValue: string | undefined) => {
+  const { PrivateKey } = await import("snarkyjs");
+  const key = customKeyValue
+    ? PrivateKey.fromBase58(customKeyValue)
+    : PrivateKey.random();
+  const publicKey = key.toPublicKey().toBase58();
+  const privateKey = key.toBase58();
+  return { publicKey, privateKey };
+};
+
+expose(generateKeys);
