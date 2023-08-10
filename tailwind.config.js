9c51195005186916cca058a1e817f7669ea6daf5
devarend <devarend@gmail.com>
2023-08-10 23:06:53 +0200

    set base to false in daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,26 +6,18 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
+    base: false,
     themes: [
       {
         mina: {
           primary: "#1f2937",
-
           secondary: "#202123",
-
           accent: "#f9682f",
-
           neutral: "#22272a",
           dark: "#131415",
-
-          // "base-100": "#eceef3",
-
           info: "#6d84d0",
-
           success: "#248f7b",
-
           warning: "#955604",
-
           error: "#f1414d",
         },
       },
