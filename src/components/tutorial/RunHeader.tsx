08c3b4801a042512b82013ef8a836cf647e07734
devarend <devarend@gmail.com>
2023-10-21 00:09:07 +0200

    created basic concepts and common methods tutorial

--- src/components/tutorial/RunHeader.tsx
+++ src/components/tutorial/RunHeader.tsx
@@ -2,7 +2,7 @@ import { FC, PropsWithChildren } from "react";
 
 const RunHeader: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <h1 className="mt-4 p-2 shadow-2xl rounded-md text-4xl font-bold leading-none tracking-tight md:text-5xl lg:text-2xl text-white bg-gradient-to-br from-pink-500 to-orange-400">
+    <h1 className="mt-4 p-2 shadow-2xl rounded-md text-4xl font-bold leading-none tracking-tight md:text-5xl lg:text-2xl text-white bg-green-600">
       {children}
     </h1>
   );
