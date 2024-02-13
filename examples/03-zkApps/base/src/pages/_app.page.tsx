7692e3ef90ea026fc0943d063981a2e4d116adb2
devarend <devarend@gmail.com>
2024-02-13 23:38:07 +0100

    updated meta json files and added base files in separate directory

--- examples/03-zkApps/base/src/pages/_app.page.tsx
+++
@@ -1,9 +0,0 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-
-import "./reactCOIServiceWorker";
-
-export default function App({ Component, pageProps }: AppProps) {
-  console.log("test1239");
-  return <Component {...pageProps} />;
-}
