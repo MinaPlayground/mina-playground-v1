8d4deb5f3de9f81dacac4fa8c679b4b482bad8cb
devarend <devarend@gmail.com>
2024-02-15 23:09:32 +0100

    added custom loader to iframe component

--- src/components/iframe/Iframe.tsx
+++ src/components/iframe/Iframe.tsx
@@ -1,19 +1,30 @@
 import { useAppSelector } from "@/hooks/useAppSelector";
 import { selectServerUrl } from "@/features/webcontainer/webcontainerSlice";
 import * as React from "react";
-import { FC } from "react";
+import { FC, useState } from "react";
+import Loader from "@/components/Loader";
 
 const Iframe: FC = () => {
   const serverUrl = useAppSelector(selectServerUrl);
+  const [loading, setIsLoading] = useState(true);
   return (
-    <iframe
-      src={serverUrl || ""}
-      className="bg-gray-100"
-      height={"100%"}
-      width={"100%"}
-      allowFullScreen
-      allow="cross-origin-isolated"
-    />
+    <div className="flex flex-1 bg-white">
+      {loading && (
+        <div className="flex flex-1 justify-center items-center">
+          <Loader
+            text="Loading preview"
+            circleColor={"text-gray-400"}
+            spinnerColor={"fill-white"}
+          />
+        </div>
+      )}
+      <iframe
+        onLoad={() => setIsLoading(false)}
+        src={serverUrl}
+        className={`bg-gray-100 flex-1 ${loading && "hidden"}`}
+        allow="cross-origin-isolated"
+      />
+    </div>
   );
 };
 
